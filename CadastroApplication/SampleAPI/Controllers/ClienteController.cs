9d641647f00373bc15e61a799ee904a01c9474c5
Bruno Melo <bruno.b.melo@live.com>
2023-06-14 13:40:13 -0300

    chore: add project created on 2023-06-13 in master class

---
+++ CadastroApplication/SampleAPI/Controllers/ClienteController.cs
@@ -0,0 +1,169 @@
+using Microsoft.AspNetCore.Mvc;
+using SampleAPI.Model;
+using SampleAPI.Repository;
+
+namespace SampleAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ClienteController : ControllerBase
+{
+    private  readonly AppDbContext _repository;
+
+    public ClienteController(AppDbContext appDbContext)
+    {
+        _repository = appDbContext;
+    }
+
+    [HttpPatch]
+    [Route("/cliente/{id}")]
+    public IActionResult Patch([FromBody]Cliente model, [FromRoute]int id)
+    {
+        try
+        {
+            var client = _repository.Clientes.Find(id);
+            if (client == null) {
+                return NotFound();
+            }
+
+            _repository.Entry(client).CurrentValues.SetValues(model);
+            var result = _repository.SaveChanges();
+            if (result > 0) {
+                return Ok();
+            }
+            
+            return BadRequest();
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+        }
+    }
+
+    [HttpPut]
+    [Route("/cliente")]
+    public IActionResult Put([FromBody]Cliente model)
+    {
+        try
+        {
+            _repository.Add(model);
+            var result = _repository.SaveChanges();
+            if (result > 0) {
+                return Ok();
+            }
+            
+            return BadRequest();
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+        }
+    }
+
+    [HttpPost]
+    [Route("/cliente")]
+    public IActionResult Post([FromBody]Cliente model)
+    {
+        try
+        {
+            _repository.Clientes.Add(model);
+            var result = _repository.SaveChanges();
+            if (result > 0) {
+                return Ok();
+            }
+            
+            return BadRequest();
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+        }
+    }
+
+    [HttpDelete]
+    [Route("/cliente/{id}")]
+    public IActionResult Delete(int id)
+    {
+        try
+        {
+            var client = _repository.Clientes.Find(id);
+            if (client == null) {
+                return NotFound();
+            }
+            
+            _repository.Clientes.Remove(client);
+            var resultado = _repository.SaveChanges();
+
+            if (resultado > 0) {
+                return Ok();
+            }
+
+            return Ok(resultado);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+        }
+    }
+
+
+    [HttpGet()]
+    [Route("/cliente/{id}")]
+    public ActionResult<Cliente> Get(int id)
+    {
+        try
+        {
+            var client = _repository.Clientes.Find(id);
+            if (client == null) {
+                return NotFound();
+            }
+
+            return Ok(client);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+        }
+    }
+
+    [HttpGet()]
+    [Route("/cliente")]
+    public ActionResult<List<Cliente>> GetAll()
+    {
+
+        try
+        {
+            var clientes = _repository.Clientes.ToList();
+            if (clientes == null || clientes.Count == 0) {
+                return NotFound();
+            }
+
+            return Ok(clientes);
+        }
+        catch (Exception ex)
+        {            
+            return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+        }
+
+    }
+
+    [HttpGet()]
+    [Route("/cliente/status")]
+    public ActionResult<List<Cliente>> GetWithQuerie([FromQuery]bool status)
+    {
+
+        try
+        {
+            var clientes = _repository.Clientes.Where(x => x.Status == status).ToList();
+            if (clientes == null || clientes.Count == 0) {
+                return NotFound();
+            }
+
+            return Ok(clientes);
+        }
+        catch (Exception ex)
+        {            
+            return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+        }
+    }
+}
---
+++ CadastroApplication/SampleAPI/Repository/AppDbContext.cs
@@ -0,0 +1,69 @@
+using Microsoft.EntityFrameworkCore;
+using SampleAPI.Model;
+
+namespace SampleAPI.Repository;
+
+public class AppDbContext : DbContext
+{
+
+    public DbSet<Cliente> Clientes { get; set; }
+
+    public AppDbContext()
+    {
+    }
+    public AppDbContext(DbContextOptions<AppDbContext> options): base(options)
+    {
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=localhost,1433;Database=Lab365;User ID=sa;Password=kalleb09321@;Trusted_Connection=False; TrustServerCertificate=True;");
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Cliente>().HasData(
+                        new Cliente
+                        {
+                            Id = 1,
+                            Nome = "Maria",
+                            Sobrenome="Fernandes",
+                            Email="maria@mail.com",
+                            Status = true,
+                        },
+                        new Cliente
+                        {
+                            Id = 2,
+                            Nome = "João",
+                            Sobrenome="Paulo",
+                            Email="jp@mail.com",
+                            Status = true,
+                        },
+                        new Cliente
+                        {
+                            Id = 3,
+                            Nome = "Martin",
+                            Sobrenome="Fowler",
+                            Email="mf@mail.com",
+                            Status = false,
+                        },
+                        new Cliente
+                        {
+                            Id = 4,
+                            Nome = "Uncle",
+                            Sobrenome="Bob",
+                            Email="martin.c.@mail.com",
+                            Status = false,
+                        },
+                        new Cliente
+                        {
+                            Id = 5,
+                            Nome = "Barbara",
+                            Sobrenome="Liskov",
+                            Email="lsp@mail.com",
+                             Status = false,
+                        }
+           );
+    }
+
+}
