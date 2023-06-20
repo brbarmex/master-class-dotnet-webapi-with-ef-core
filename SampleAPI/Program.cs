7113d4a51ff1788383918c04308a43ecfa4da7e1
Bruno Melo <bruno.b.melo@live.com>
2023-06-20 20:32:34 -0300

    add: fixes

--- SampleAPI/Program.cs
+++ SampleAPI/Program.cs
@@ -15,15 +15,17 @@ public class Program
         // Add services to the container.
 
         builder.Services.AddControllers();
+        builder.Services.AddControllers().AddJsonOptions(x =>
+        {
+            x.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter(JsonNamingPolicy.CamelCase));
+        });
+
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
 
         builder.Services.AddAutoMapper(typeof(Program));
 
-       // builder.Services.AddControllers().AddNewtonsoftJson(x => x.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
-
-        builder.Services.AddControllers().AddJsonOptions(x =>{ x.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter(JsonNamingPolicy.CamelCase)); });
 
         //To EF
         builder.Services.AddScoped<AppDbContext>();
